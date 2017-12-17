cad65ab5ae5d8cb8d002e5a047ba7ee9c9208f9a
Laurent Perroteau <l.perroteau@sbt-human.com>
2017-12-17 17:11:42 +0100

    Add chat service/component and socket.io client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
+
+import { SocketIoModule, SocketIoConfig } from 'ng2-socket-io';
+
+import { ChatService } from './chat.service';
 
 import { AppComponent } from './app.component';
-
+import { ChatComponent } from './chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
-  providers: [],
+  providers: [ChatService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
